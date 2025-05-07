0187e77f6e8d6b67ae249334af958d9a0716f3c5
Dawid Stasiak <dawid.stasiak21@gmail.com>
2025-05-07 19:37:47 +0200

    err-ndet

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,5 +4,5 @@ rebar3 as prod get-deps
 docker build -t mapreduce/erlangmapreduce -f Dockerfile .
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v4e3t3o3
-docker tag mapreduce/erlangmapreduce:latest public.ecr.aws/v4e3t3o3/mapreduce/erlangmapreduce:latest
-docker push public.ecr.aws/v4e3t3o3/mapreduce/erlangmapreduce:latest
+docker tag mapreduce/erlangmapreduce:latest public.ecr.aws/v4e3t3o3/mapreduce/erlangmapreduce:err-ndet
+docker push public.ecr.aws/v4e3t3o3/mapreduce/erlangmapreduce:err-ndet
