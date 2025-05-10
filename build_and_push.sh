0304c6e2705fb25d959f70598ef0d1143304bcfd
Dawid Stasiak <dawid.stasiak21@gmail.com>
2025-05-10 16:58:01 +0200

    err-det50

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,5 +4,5 @@ rebar3 as prod get-deps
 docker build -t mapreduce/erlangmapreduce -f Dockerfile .
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/v4e3t3o3
-docker tag mapreduce/erlangmapreduce:latest public.ecr.aws/v4e3t3o3/mapreduce/erlangmapreduce:err-det
-docker push public.ecr.aws/v4e3t3o3/mapreduce/erlangmapreduce:err-det
+docker tag mapreduce/erlangmapreduce:latest public.ecr.aws/v4e3t3o3/mapreduce/erlangmapreduce:err-det50
+docker push public.ecr.aws/v4e3t3o3/mapreduce/erlangmapreduce:err-det50
